9e41649c83d3666568359e9d8a4dd9f97f32a43e
Vinod Kottamasu <vinod.kottamasu@xcubelabs.com>
2023-01-04 22:21:17 +0530

    updated android sdk

--- example/lib/home_screen.dart
+++ example/lib/home_screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      FlutterUpshotPlugin.showActivity(-1, 'exitIntent', context);
+      FlutterUpshotPlugin.showActivity(-1, '', context);
     });
   }
 
